e48b006d8e8374d9b3cb6ac6b6ad0581b2c424f2
Suphakrit Nakhabat <suphakrit.bas@gmail.com>
2017-08-28 22:17:21 +0700

    new club and new player

--- routes/api/clubs.js
+++ routes/api/clubs.js
@@ -1,20 +1,50 @@
 const router = require('express').Router();
 const Club = require('../../models/club');
 
-router.get('/clubs', function(req, res) {
-  res.json({message: 'GET /clubs'});
-});
+// router.get('/clubs', function(req, res) {
+//     res.json({ message: 'GET /clubs' });
+// });
 
 router.get('/clubs/:id', (req, res) => {
-  res.json({message: `GET /clubs/${req.params.id}`});
+    res.json({ message: `GET /clubs/${req.params.id}` });
+});
+
+// router.post('/clubs/', (req, res) => {
+//     res.json({ message: 'POST /club', data: req.body });
+// });
+
+router.delete('/clubs/:id', (req, res) => {
+    res.json({ message: `DELETE /clubs/${req.params.id}` });
 });
 
 router.post('/clubs/', (req, res) => {
-  res.json({message: 'POST /club', data: req.body });
+    let playload = req.body;
+    let newClub = new Club({
+        name: playload.name,
+        country: playload.country
+    });
+    newClub
+        .save()
+        .then(data => {
+            res.json({
+                data: data
+            });
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+        });
 });
 
-router.delete('/clubs/:id', (req, res) => {
-  res.json({message: `DELETE /clubs/${req.params.id}`});
+router.get('/clubs', (req, res) => {
+    Club
+        .find()
+        .populate({ path: 'club', select: 'name' })
+        .then(data => {
+            res.json({ data: data });
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+        });
 });
 
 module.exports = router;
--- routes/api/players.js
+++ routes/api/players.js
@@ -1,20 +1,62 @@
 const router = require('express').Router();
 const Player = require('../../models/player');
+const mongoose = require('mongoose');
 
-router.get('/players', function(req, res) {
-  res.json({message: 'GET /players'});
-});
+// router.get('/players', function(req, res) {
+//     res.json({ message: 'GET /players' });
+//     Player.find()
+//         .then(function(data) {
+//             res.json({
+//                 data: data
+//             });
+//             console.log('data', data);
+//         });
+// });
 
 router.get('/players/:id', (req, res) => {
-  res.json({message: `GET /players/${req.params.id}`});
+    res.json({ message: `GET /players/${req.params.id}` });
+});
+
+// router.post('/players/', (req, res) => {
+//     res.json({ message: 'POST /players', data: req.body });
+// });
+
+router.delete('/players/:id', (req, res) => {
+    res.json({ message: `DELETE /players/${req.params.id}` });
 });
 
 router.post('/players/', (req, res) => {
-  res.json({message: 'POST /players', data: req.body });
+    let payload = req.body;
+    let newPlayer = new Player({
+        name: payload.name,
+        position: payload.position,
+        age: payload.age,
+        club: mongoose.Types.ObjectId(payload.club)
+    });
+    newPlayer
+        .save()
+        .then(function(data) {
+            res.json({
+                data: data
+            });
+        })
+        .catch(function(error) {
+            res.status(500).json({ error: error.message });
+        });
 });
 
-router.delete('/players/:id', (req, res) => {
-  res.json({message: `DELETE /players/${req.params.id}`});
+router.get('/players', function(req, res) {
+    Player
+        .find()
+        .populate({ path: 'club', select: 'name' })
+        .then(function(data) {
+            res.json({
+                data: data
+            });
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+        });
 });
 
 module.exports = router;
